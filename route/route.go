333794f2ae1dab2f816afcaab0e37b1b8eb5786d
Kongoole <1160104271@qq.com>
2018-06-20 11:02:00 +0800

    fix file server bug

--- route/route.go
+++ route/route.go
@@ -9,10 +9,10 @@ func Register() {
 	http.HandleFunc("/", controller.Home{}.Index)
 	http.HandleFunc("/blog", controller.Blog{}.Index)
 	http.HandleFunc("/article", controller.Blog{}.Article)
-	http.HandleFunc("/public", serveResource)
+	http.HandleFunc("/public/", serveResource)
 }
 
 // serveResource returns static files
 func serveResource(w http.ResponseWriter, r *http.Request) {
-	http.StripPrefix("/public", http.FileServer(http.Dir("public")))
+	http.StripPrefix("/public/", http.FileServer(http.Dir("public"))).ServeHTTP(w, r)
 }
